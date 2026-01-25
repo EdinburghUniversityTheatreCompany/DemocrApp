e2ca20a7a2a7ca75b0e3af9e71ddb3aa7dc81ab1
Mick Zijdel <mickzijdel@hotmail.com>
2026-01-25 12:19:23 +0000

    feat: Add json and yaml meeting reports

--- DemocrApp-API/Meeting/views/reports/__init__.py
+++ DemocrApp-API/Meeting/views/reports/__init__.py
@@ -1,3 +1,3 @@
-from .meeting_report import meeting_report
+from .meeting_report import meeting_report, meeting_report_json, meeting_report_yaml
 from .vote_report import vote_report
 from .report_list import report_list
--- DemocrApp-API/Meeting/views/reports/meeting_report.py
+++ DemocrApp-API/Meeting/views/reports/meeting_report.py
@@ -1,3 +1,5 @@
+import yaml
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from ...models import Meeting, Vote
 
@@ -7,4 +9,53 @@ def meeting_report(request, meeting_id):
     meeting = get_object_or_404(Meeting, pk=meeting_id)
     context['meeting'] = meeting
     context['votes'] = Vote.objects.filter(token_set__meeting=meeting)
+    context['token_sets'] = meeting.tokenset_set.all()
     return render(request, 'meeting/reports/meeting.html', context)
+
+
+def _build_meeting_data(meeting):
+    """Build structured data dict for a meeting and its votes."""
+    votes = Vote.objects.filter(token_set__meeting=meeting)
+
+    votes_data = []
+    for vote in votes:
+        vote_data = {
+            "id": vote.id,
+            "name": vote.name,
+            "method": vote.method,
+            "method_display": vote.get_method_display(),
+            "responses": vote.responses(),
+            "options": [
+                {"id": opt.id, "name": opt.name}
+                for opt in vote.option_set.all()
+            ],
+            "results": vote.results_data or {},
+        }
+        votes_data.append(vote_data)
+
+    return {
+        "meeting": {
+            "id": meeting.id,
+            "name": meeting.name,
+            "time": meeting.time.isoformat() if meeting.time else None,
+            "close_time": meeting.close_time.isoformat() if meeting.close_time else None,
+        },
+        "votes": votes_data,
+    }
+
+
+def meeting_report_json(request, meeting_id):
+    meeting = get_object_or_404(Meeting, pk=meeting_id)
+    data = _build_meeting_data(meeting)
+    response = JsonResponse(data, json_dumps_params={'indent': 2})
+    response['Content-Disposition'] = f'attachment; filename="meeting_{meeting_id}_report.json"'
+    return response
+
+
+def meeting_report_yaml(request, meeting_id):
+    meeting = get_object_or_404(Meeting, pk=meeting_id)
+    data = _build_meeting_data(meeting)
+    yaml_content = yaml.dump(data, default_flow_style=False, allow_unicode=True, sort_keys=False)
+    response = HttpResponse(yaml_content, content_type='application/x-yaml')
+    response['Content-Disposition'] = f'attachment; filename="meeting_{meeting_id}_report.yaml"'
+    return response
