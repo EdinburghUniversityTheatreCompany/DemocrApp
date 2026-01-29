15b335fab1c04d24bd4e0eeef248ccc59b251a1f
Mick Zijdel <mickzijdel@hotmail.com>
2026-01-29 17:10:09 +0000

    deps: Update python packages and fix Django 6.0 deprecated format_html

--- DemocrApp-API/Meeting/templatetags/vote_helpers.py
+++ DemocrApp-API/Meeting/templatetags/vote_helpers.py
@@ -11,23 +11,28 @@ register = template.Library()
 def vote_action_button(vote):
     args = [vote.token_set.meeting_id, vote.id]
     if vote.method == Vote.STV and vote.state == Vote.LIVE:
-        return format_html("<a class='btn btn-sm btn-warning' href='{}'>{}</a>".format(
+        return format_html("<a class='btn btn-sm btn-warning' href='{}'>{}</a>",
             reverse('meeting/close_vote/stv', args=args),
-            "Close Vote"))
-    cases = {
-        Vote.READY: "<a class='btn btn-sm btn-success' href='{}'>{}</a>".format(
+            "Close Vote")
+
+    if vote.state == Vote.READY:
+        return format_html("<a class='btn btn-sm btn-success' href='{}'>{}</a>",
             reverse('meeting/open_vote', args=args),
-            "Open Vote"),
-        Vote.LIVE: "<a class='btn btn-sm btn-warning' href='{}'>{}</a>".format(
+            "Open Vote")
+    elif vote.state == Vote.LIVE:
+        return format_html("<a class='btn btn-sm btn-warning' href='{}'>{}</a>",
             reverse('meeting/close_vote', args=args),
-            "Close Vote"),
-        Vote.COUNTING: "Counting",
-        Vote.NEEDS_TIE_BREAKER: "<a class='btn btn-sm btn-secondary' href='{}'>{}</a>".format(
+            "Close Vote")
+    elif vote.state == Vote.COUNTING:
+        return format_html("{}", "Counting")
+    elif vote.state == Vote.NEEDS_TIE_BREAKER:
+        return format_html("<a class='btn btn-sm btn-secondary' href='{}'>{}</a>",
             reverse('meeting/break_tie', args=args),
-            "Needs Tie Breaker"),
-        Vote.CLOSED: "-",
-    }
-    return format_html(cases[vote.state])
+            "Needs Tie Breaker")
+    elif vote.state == Vote.CLOSED:
+        return format_html("{}", "-")
+
+    return format_html("{}", "")
 
 
 @register.simple_tag(name="vote_responses_or_remove")
@@ -46,7 +51,6 @@ def vote_responses_or_remove(vote, token):
 
 @register.simple_tag(name="option_remove_button")
 def option_remove_button(option):
-    out = ""
     if option.vote.method != Vote.YES_NO_ABS and option.vote.state == Vote.READY:
-        out = "<button class='btn btn-sm btn-danger m-1' type='button' onclick='remove_option({})'>remove</button>".format(option.id)
-    return format_html(out)
+        return format_html("<button class='btn btn-sm btn-danger m-1' type='button' onclick='remove_option({})'>remove</button>", option.id)
+    return format_html("{}", "")
